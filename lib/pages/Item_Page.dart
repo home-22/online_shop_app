1078f781b758f22847e08fb89bf61d155943102d
home-22 <armin.jasarevic.25@gmail.com>
2022-08-20 23:29:19 +0200

    online_shop_app

--- lib/pages/Item_Page.dart
+++ lib/pages/Item_Page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:online_shop_app/widgets/CartAppBar.dart';
 import 'package:online_shop_app/widgets/ItemAppBar.dart';
 
 class ItemPage extends StatelessWidget {
@@ -6,15 +8,60 @@ class ItemPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: ListView(
-        children: [
-          const ItemAppBar(),
-          Padding(
-            padding: const EdgeInsets.all(0),
-          ),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            const ItemAppBar(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+              child: Row(
+                children: const [
+                  Text(
+                    'Pink Blazer',
+                    style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  Text(
+                    '\$250',
+                    style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30),
+              child: Row(
+                children: const [
+                  Text(
+                      'Pink blazer with soft matrial.not hot comfortable\ncaying.available in various sizes.suitable for use at\nparties'),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10, left: 20, top: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RatingBar.builder(
+                    initialRating: 4,
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    itemCount: 5,
+                    itemSize: 20,
+                    itemPadding: const EdgeInsets.symmetric(horizontal: 4),
+                    itemBuilder: (context, index) => const Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                    ),
+                    onRatingUpdate: (index) {},
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/ItemAppBar.dart
+++ lib/widgets/ItemAppBar.dart
@@ -6,38 +6,43 @@ class ItemAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      padding: EdgeInsets.all(25),
-      child: Row(
-        children: [
-          InkWell(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: Icon(
-              Icons.arrow_back,
-              size: 30,
-              color: Colors.red[300],
-            ),
+      height: 400,
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(50),
+              bottomRight: Radius.circular(50)),
+          image: DecorationImage(
+              image: AssetImage('images/3.jpg'), fit: BoxFit.cover)),
+      child: Positioned(
+          child: Column(children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          child: Row(
+            children: [
+              InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: const CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.arrow_circle_left_outlined,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+              const Spacer(),
+              const CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.favorite_border,
+                  color: Colors.red,
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: Text(
-              'Product',
-              style: TextStyle(
-                  fontSize: 23,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.red[300]),
-            ),
-          ),
-          const Spacer(),
-          const Icon(
-            Icons.favorite,
-            size: 30,
-            color: Colors.red,
-          )
-        ],
-      ),
+        ),
+      ])),
     );
   }
 }
